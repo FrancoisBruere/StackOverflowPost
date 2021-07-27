91ff74569c964d77b19a79a3113cf085e97e42c1
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-07-27 12:46:05 +0200

    Add project files.

---
+++ Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    public class Post
+    {
+        public string postTitle { get; set; }
+        public string postDescription { get; set; }
+        public DateTime postDateTime { get; set; }
+
+        public int _vote { get; private set; }
+
+        public void VoteUp(int voteup)
+        {
+            _vote += voteup;
+            Console.WriteLine("Vote added! Vote count: " + _vote);
+            
+
+        }
+
+
+        public void VoteDown(int votedown)
+        {
+
+            _vote -= votedown;
+            Console.WriteLine("Vote removed! Vote count: " + _vote);
+            
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var post = new Post();
+
+            Console.Write("Enter your post title: ");
+            post.postTitle = Console.ReadLine();
+           
+            Console.Write("Enter post description: ");
+            post.postDescription = Console.ReadLine();
+            post.postDateTime = DateTime.Now;
+
+            
+            while (true)
+            {
+                Console.Write("Vote up press (U) vote down press (D) to quit press (Q)");
+                var input = Console.ReadLine();
+                if (input.ToLower() == "u")
+                {
+                    post.VoteUp(1);
+                    
+                }
+                if (input.ToLower() == "d")
+                {
+                    post.VoteDown(1);
+                    
+                }
+                if (input.ToLower() == "q")
+                {
+                    break;
+                }
+               
+            }
+            
+
+
+
+        }
+    }
+}
